e99d586a3016e8316cccf9493a7ca287f374b48f
inaraahmadova <tu7r5hs82@code.edu.az>
2024-04-06 00:52:33 +0400

    first commit

---
+++ class5aprel(1)/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace class5aprel_1_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Student student = new Student();
+            //student.SayHi("inara");
+            //student.SayHi("inara");
+            //Student.Pi = 5254f;
+
+        }
+    }
+}
---
+++ class5aprel(1)/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class5aprel_1_
+{
+    internal class Student
+    {
+        public static float Pi { get;} = 65541f;
+        static Student()
+        {
+            Pi = 1.0f;
+        }
+        public void SayHi(string name)
+        {
+            Console.WriteLine("salam " + name);
+        }
+
+        //static class Person:Student{}
+        public string Name { get; set; }
+        public static void SayHi()
+        {
+            Console.WriteLine("ahgfdhg");
+        }
+    }
+}
